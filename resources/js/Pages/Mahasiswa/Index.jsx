ea344e29d1b60e6bd90c7a07b3ace9f38b98c04b
abrarradan30 <abradan84@gmail.com>
2024-10-30 13:38:45 +0700

    configuration and make data table

---
+++ resources/js/Pages/Mahasiswa/Index.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+export default function Index({mahasiswa}) {
+    console.log(mahasiswa);
+    return (
+        <div>
+            <h3>Data Mahasiswa</h3>
+            <hr />
+            <table cellPadding={5} border={1} style={{
+                borderCollapse: 'collapse'
+            }}>
+                <thead>
+                    <tr>
+                        <th>No.</th>
+                        <th>NIM</th>
+                        <th>Nama Lengkap</th>
+                        <th>Jenis Kelamin</th>
+                        <th>Alamat</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        mahasiswa.length === 0 ? (
+                            <tr>
+                                <th colSpan={5}>Data kosong ...</th>
+                            </tr>
+                        ) : (mahasiswa.map((mhs, index) => (
+                            <tr key={index}>
+                                <td>{index+1}</td>
+                                <td>{mhs.nim}</td>
+                                <td>{mhs.nama_lengkap}</td>
+                                <td>
+                                    {mhs.jenkel == 'L' ? 'Laki-Laki' : 'Perempuan'}
+                                </td>
+                                <td>{mhs.alamat}</td>
+                            </tr>
+                        )) 
+                        )
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
+}
---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan CRUD Inertia React JS</title>
+    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
+</head>
+<body>
+    <div id="app" data-page="{{ json_encode($page) }}"></div>
+
+    @viteReactRefresh
+</body>
+</html>
